3436c382f60d0dc69e6e7ffc1594aaba4ce1c902
Luis Gustavo <gustaavojaco@gmail.com>
2021-12-06 14:29:40 -0300

    Listagem de usuários finalizada

--- administracao/views/usuario_views.py
+++ administracao/views/usuario_views.py
@@ -1,5 +1,6 @@
 from ..forms.usuario_forms import UsuarioForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model
 
 # Cadastrando um usuário
 def cadastrar_usurario(request):
@@ -7,6 +8,15 @@ def cadastrar_usurario(request):
         form_usuario = UsuarioForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
+            return redirect('listar_usuarios')
     else:
         form_usuario = UsuarioForm()
     return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
+
+
+# Listando os usuários administradores cadastrados
+def listar_usuarios(request):
+    User = get_user_model()
+    usuarios = User.objects.filter(is_superuser=True)
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
+
